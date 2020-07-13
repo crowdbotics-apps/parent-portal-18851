8ba8a5a54e3f857b2f5a3965447a5eb5fa9a264e
Crowdbotics <team@crowdbotics.com>
2020-07-13 23:08:36 +0000

    Update models

--- backend/home/migrations/0003_homepage_app.py
+++ backend/home/migrations/0003_homepage_app.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0002_load_initial_data'),
+        ("home", "0002_load_initial_data"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='homepage',
-            name='app',
+            model_name="homepage",
+            name="app",
             field=models.URLField(blank=True, null=True),
         ),
     ]
